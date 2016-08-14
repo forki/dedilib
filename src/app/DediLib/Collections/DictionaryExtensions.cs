b1606f590a848e5f4f3d8d77eb22c2d155b62c13
Hans Wolff <hanswolff@users.noreply.github.com>
2016-08-14 09:49:55 +0200

    added extension methods for dictionaries

---
+++ src/app/DediLib/Collections/DictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DediLib.Collections
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> createFunc)
+        {
+            if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (createFunc == null) throw new ArgumentNullException(nameof(createFunc));
+
+            TValue result;
+            if (dictionary.TryGetValue(key, out result))
+                return result;
+
+            result = createFunc(key);
+            dictionary[key] = result;
+            return result;
+        }
+    }
+}
